bb7f9b318ffac2bd458a229733be410f739ff3d4
Kyle Rush <kdr213@gmail.com>
2018-06-02 23:33:46 +0000

    changed address of pi

--- middleman/routes/update.js
+++ middleman/routes/update.js
@@ -3,11 +3,11 @@ var router = express.Router();
 var request = require('request');
 
 //IP ADDRESS OF TICKET COUNTER
-var ticketCounterAddress = '10.0.4.10:3000/update';
+var ticketCounterAddress = 'http://10.0.4.10:3000/update';
 /* GET update page. */
 router.get('/', function(req, res, next) {
 	request(ticketCounterAddress, function(error, response, body) {
-		console.log("Requested: " + ticketCounterAddress + "\nBody: " + body);
+		console.log("Requested: " + ticketCounterAddress + "\nBody: " + body + "\nError" + error);
 		res.send(200, "You've engaged the middleman, and received this response from your destination: " + body);
 	});
 });
