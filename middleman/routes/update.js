760a669fbf07c010ba9fcca420de553d4da27f17
Kyle Rush <krush@sonobello.com>
2018-06-02 13:56:07 -0700

    changing var name in middleman router

--- middleman/routes/update.js
+++ middleman/routes/update.js
@@ -7,7 +7,7 @@ var ticketCounterAddress = '10.0.4.10:3000/update';
 /* GET update page. */
 router.get('/', function(req, res, next) {
 	request(ticketCounterAddress, function(error, response, body) {
-		console.log("Requested: " + ticketCounterIP + "\nBody: " + body);
+		console.log("Requested: " + ticketCounterAddress + "\nBody: " + body);
 		res.send(200, "You've engaged the middleman, and received this response from your destination: " + body);
 	});
 });
