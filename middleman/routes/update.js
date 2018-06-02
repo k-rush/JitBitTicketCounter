d852768c7f35ebd0738680654d674b028cffbd22
Kyle Rush <krush@sonobello.com>
2018-06-01 17:41:59 -0700

    dns record to krush-pi.sonobelloapps.com

--- middleman/routes/update.js
+++ middleman/routes/update.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var request = require('request');
 
 //IP ADDRESS OF TICKET COUNTER
-var ticketCounterIP = '10.0.4.10';
+var ticketCounterAddress = 'krush-pi.sonobelloapps.com/update';
 /* GET update page. */
 router.get('/', function(req, res, next) {
 	request(ticketCounterIP, function(error, response, body) {
